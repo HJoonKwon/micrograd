ae2a65330a55b5b42414a20267fd10cdce0c0e26
HJoonKwon <dsazz801@gmail.com>
2023-12-30 23:19:22 +0900

    update: backward funcs and topological sorting for entire backward process

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -16,13 +16,25 @@ class Value:
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, _children=(self, other), _op="+")
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+
+        out._backward = _backward
         return out
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, _children=(self, other), _op="*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
         return out
-    
+
     def __neg__(self):
         return self * -1
 
@@ -30,34 +42,67 @@ class Value:
         return self + (-other)
 
     def __pow__(self, other):
-        assert isinstance(other, (int, float)), "Not supported type. Only int/float are supported for now"
-        out = Value(self.data ** other, _children=(self, other), _op=f"**{other}")
-        return out 
-    
+        assert isinstance(
+            other, (int, float)
+        ), "Not supported type. Only int/float are supported for now"
+        out = Value(self.data**other, _children=(self,), _op=f"**{other}")
+
+        def _backward():
+            self.grad += other * self.data ** (other - 1) * out.grad
+
+        out._backward = _backward
+        return out
+
     def __truediv__(self, other):
         return self * other**-1
 
     def __radd__(self, other):
-        return self + other 
+        return self + other
 
     def __rsub__(self, other):
         return -self + other
 
     def __rmul__(self, other):
-        return self * other 
-    
+        return self * other
+
     def exp(self):
         out = Value(math.exp(self.data), _children=(self,), _op="exp")
+
+        def _backward():
+            self.grad += math.exp(self.data) * out.grad
+
+        out._backward = _backward
         return out
 
     def tanh(self):
         exp = self.exp()
-        negexp = (-self).exp() 
+        negexp = (-self).exp()
         return (exp - negexp) / (exp + negexp)
 
     def relu(self):
         out = Value(0 if self.data < 0 else self.data, _children=(self,), _op="ReLU")
-        return out  
+
+        def _backward():
+            self.grad += 0 if self.data < 0 else out.grad
+
+        out._backward = _backward
+        return out
 
     def backward(self):
-        pass
+        self.grad = 1.0
+        visit = set()
+        topo = []
+
+        def topo_sort(n):
+            assert n != -1, f"weird n = {n}, topo={topo}"
+            if n in visit:
+                return
+            visit.add(n)
+            for child in n._prev:
+                topo_sort(child)
+            topo.append(n)
+
+        topo_sort(self)
+        topo_rev = reversed(topo)
+        for n in topo_rev:
+            n._backward()
