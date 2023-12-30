ecc105aff39e3646db0a0446d37845987d6a77e7
HJoonKwon <dsazz801@gmail.com>
2023-12-30 22:28:55 +0900

    update: unit test for Value ops

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,6 +1,21 @@
-import torch 
-from micrograd.engine import Value 
+import torch
+import numpy as np
+from micrograd.engine import Value
 
-def test_sanity_check():
+
+def test_ops():
     a = Value(-4.0)
-    
+    b = Value(8.0)
+
+    a_ = torch.tensor([-4.0])
+    b_ = torch.tensor([8.0])
+
+    assert np.allclose((a + b).data, (a_ + b_).item())
+    assert np.allclose((a * b).data, (a_ * b_).item())
+    assert np.allclose((a**b.data).data, (a_**b_).item())
+    assert np.allclose(
+        (a.tanh() * b.exp()).data, (torch.tanh(a_) * torch.exp(b_)).item()
+    )
+    assert np.allclose(
+        ((5 + a) + (5 - b) * (6 * a)).data, ((5 + a_) + (5 - b_) * (6 * a_)).item()
+    )
