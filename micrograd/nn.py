8c2264bf5c2dcd8ce8661d216f7776aaf3609262
HJoonKwon <dsazz801@gmail.com>
2023-12-30 23:59:43 +0900

    update: nn modules and unit test for forward pass

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -12,33 +12,45 @@ class Module:
 
 
 class Neuron(Module):
-    def __init__(self):
-        pass
+    def __init__(self, nin, nonlin=True):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+        self.nonlin = nonlin
 
-    def __call__(self):
-        pass
+    def __call__(self, x):
+        assert len(x) == len(self.w)
+        acts = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+        return acts.relu() if self.nonlin else acts
 
     def parameters(self):
-        pass
+        return self.w + [self.b]
 
 
 class Layer(Module):
-    def __init__(self):
-        pass
+    def __init__(self, nin, nouts, **kwargs):
+        self.neurons = [Neuron(nin, **kwargs) for _ in range(nouts)]
 
-    def __call__(self):
-        pass
+    def __call__(self, x):
+        outs = [neuron(x) for neuron in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
 
     def parameters(self):
-        pass
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
 
 class MLP(Module):
-    def __init__(self):
-        pass
+    def __init__(self, nin, nouts):
+        nouts = nouts if isinstance(nouts, list) else list(nouts)
+        sizes = [nin] + nouts
+        self.layers = [
+            Layer(sizes[i], sizes[i + 1], nonlin=i != len(nouts) - 1)
+            for i in range(len(nouts))
+        ]
 
-    def __call__(self):
-        pass
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
     def parameters(self):
-        pass
+        return [p for layer in self.layers for p in layer.parameters()]
--- test/test_nn.py
+++ test/test_nn.py
@@ -1,1 +1,19 @@
+import torch 
 from micrograd.nn import Neuron, Layer, MLP
+
+def test_forward():
+    nin = 3 
+    nouts = [4, 4, 3, 1]
+    mlp = MLP(nin, nouts)
+    xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0]
+    ] 
+    ys = [] 
+    for x in xs:
+        y = mlp(x)
+        ys.append(y)
+    
+    
